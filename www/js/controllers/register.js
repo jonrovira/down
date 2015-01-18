afe9e271952f4bafbd6647b8cc782171af590fec
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-01-17 20:58:06 -0500

    different POST request included, but commented out

--- www/js/controllers/register.js
+++ www/js/controllers/register.js
@@ -27,30 +27,62 @@ angular.module('down')
 					lastName: $scope.lastName,
 					number: $scope.number,
 					password: $scope.password
-				}
-				// $http
-				// 	.post('/register', newUser)
-				// 	.success(function(data, status, headers, config) {
-				// 		if (!data.status) {
-				// 			$ionicLoading.hide();
-				// 			// var errorPopup = $ionicPopup.alert({
-				// 			// 	title: 'Error signing up!',
-				// 			// 	template: 'Please retry.'
-				// 			// });
-				// 			// alertPopup.then(function(res) {
-				// 			// 	console.log('Register error occurred!');
-				// 			// });
+				};
+
+				// $http({
+				// 	method: 'POST',
+				// 	url: 'https://afternoon-fjord-7983.herokuapp.com/register',
+				// 	data: newUser,
+				// 	headers: {
+				// 		'Access-Control-Allow-Origin': '*',
+    //             		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    //             		'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
 				// 		}
+				// 	})
+				// 	.success(function(data, status, headers, config) {
+				// 		console.log(data);
+				// 		if (data.status !== 'success') {
+				// 			$ionicLoading.hide();
+				// 			var errorPopup = $ionicPopup.alert({
+				// 				title: 'Error signing up!',
+				// 				template: 'Please retry.'
+				// 			});
+				// 			alertPopup.then(function(res) {
+				// 				console.log('Register error occurred!');
+				// 			});
+				// 		}
+				// 		console.log(data.token);
 				// 		window.localStorage.setItem('token', data.token);
 				// 		console.log('Successfully registered');
 				// 		$rootScope = newUser;
-						// console.log(newUser);
-						// $ionicLoading.hide();
-						// $location.url('/');
+				// 		$ionicLoading.hide();
+				// 		$location.path('/');
 				// 	});
+
+					
 				console.log(newUser);
-				$ionicLoading.hide();
-				$location.url('/');
-			}
+				$http
+					.post('https://afternoon-fjord-7983.herokuapp.com/register', newUser)
+					.success(function(data, status, headers, config) {
+						console.log(data);
+						if (data.status !== 'success') {
+							$ionicLoading.hide();
+							var errorPopup = $ionicPopup.alert({
+								title: 'Error signing up!',
+								template: 'Please retry.'
+							});
+							alertPopup.then(function(res) {
+								console.log('Register error occurred!');
+							});
+							return;
+						}
+						console.log(data.token);
+						window.localStorage.setItem('token', data.token);
+						console.log('Successfully registered');
+						$rootScope = newUser;
+						$ionicLoading.hide();
+						$location.path('/');
+					});
+			 }
 			
 	}]);
