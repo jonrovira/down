7172c3d7ab57a5a3fbb160b9efdb65bbf8595669
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-01-18 10:04:48 -0500

    friends in groups being rendered dynamically, groups and friends being correctly called from backend

--- www/js/controllers/tabs.js
+++ www/js/controllers/tabs.js
@@ -15,9 +15,40 @@ angular.module('down')
 					result[i] = '';
 				}
 				return result;
-			}
+			};
 
+			// format the array of names so that it can be rendered correctly by the template engine
+			var formatNamesArray = function(array) {
+				var result = [];
+				for (var i = 0; i < array.length; i++) {
+					result.push(array[i].first_name + ' ' + array[i].last_name);
+				}
+				return result;
+			};
 
+			// send a HTTP GET request to obtain the members of a group, given the groups array ($scope.groups) and an index into the array
+			var getFriendList = function(groups, index) {
+				// get group ID from the array of groups
+				var g_id = groups[index]._id.$oid;
+				// get friends in a certain group given a group ID
+				$http
+					.get('https://afternoon-fjord-7983.herokuapp.com/members?group_id=' + g_id)
+					.success(function(data, status, headers, configs) {
+						// console.log(data);
+						if (!data) {
+							console.log('Error status: ' + data.status);
+						} else {
+							console.log(formatNamesArray(data.members));
+							var formattedNames = formatNamesArray(data.members);
+							$scope.friends = formattedNames;
+							// no need for return since $scope.friends is being set by this function
+							//return formattedNames;
+						}
+					});
+
+			};
+
+			// get all groups of a certain user, given a user ID
 			$http
 				.get('https://afternoon-fjord-7983.herokuapp.com/groups?user_id=' + id)
 				.success(function(data, status, headers, config) {
@@ -26,14 +57,16 @@ angular.module('down')
 						console.log('Error occurred: ' + data.status);
 					} else {
 						$scope.groups = data.groups;
+						// current group = first group by default
+						$scope.currentGroup = data.groups[0];
+						getFriendList(data.groups, 0);
 						$scope.activeTabs = createTabArray(data.groups.length);
 					}
 				});
 
-			// $scope.activeTabs = ['', '', '', 'active', '', '', ''];
-
 			// function called whenever a tab is clicked
 			$scope.toggleClass = function(num) {
+				$scope.currentGroup = $scope.groups[num];
 				for (var i = 0; i < $scope.activeTabs.length; i++) {
 					if (i !== num) {
 						$scope.activeTabs[i] = '';
@@ -41,17 +74,6 @@ angular.module('down')
 						$scope.activeTabs[i] = 'active';
 					}
 				}
-
-				// get friends in a certain group
-				$http
-					.get('https://afternoon-fjord-7983.herokuapp.com/')
-					.success(function(data, status, headers, configs) {
-						if (data.status !== 'success') {
-							console.log('Error status: ' + data.status);
-						} else {
-							$scope.friends = data.friends;
-						}
-					});
-
+				getFriendList($scope.groups, num);				
 			}
 	}]);
