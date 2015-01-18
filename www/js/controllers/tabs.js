40f93bd811b6825fb10558e0d775604c5849b62d
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-01-18 09:17:54 -0500

    tabs page class change working, groups loading dynamically from backend

--- www/js/controllers/tabs.js
+++ www/js/controllers/tabs.js
@@ -4,5 +4,54 @@
 angular.module('down')
 	.controller('TabsCtrl', ['$scope', '$http', '$location', '$rootScope',
 		function($scope, $http, $rootScope, $location) {
+			// on page load, we need to get the list of groups and render them onto the main page
+			var id = window.localStorage.getItem('token');
+			var createTabArray = function(size) {
+				var result = [];
+				// set first tab as active by default
+				result[0] = 'active';
+				// set others as empty string (no class)
+				for (var i = 1; i < size; i++) {
+					result[i] = '';
+				}
+				return result;
+			}
 
+
+			$http
+				.get('https://afternoon-fjord-7983.herokuapp.com/groups?user_id=' + id)
+				.success(function(data, status, headers, config) {
+					console.log(data);
+					if (!data) {
+						console.log('Error occurred: ' + data.status);
+					} else {
+						$scope.groups = data.groups;
+						$scope.activeTabs = createTabArray(data.groups.length);
+					}
+				});
+
+			// $scope.activeTabs = ['', '', '', 'active', '', '', ''];
+
+			// function called whenever a tab is clicked
+			$scope.toggleClass = function(num) {
+				for (var i = 0; i < $scope.activeTabs.length; i++) {
+					if (i !== num) {
+						$scope.activeTabs[i] = '';
+					} else {
+						$scope.activeTabs[i] = 'active';
+					}
+				}
+
+				// get friends in a certain group
+				$http
+					.get('https://afternoon-fjord-7983.herokuapp.com/')
+					.success(function(data, status, headers, configs) {
+						if (data.status !== 'success') {
+							console.log('Error status: ' + data.status);
+						} else {
+							$scope.friends = data.friends;
+						}
+					});
+
+			}
 	}]);
